023da2542afaf45b46f9f65f5ab335289b7bfdfb
adnant1 <adnantayib8@gmail.com>
2025-09-16 17:51:30 -0400

    Tokenized user input in repl

--- repl.go
+++ repl.go
@@ -15,8 +15,13 @@ func startRepl() {
 
 		scanner.Scan()
 		text := scanner.Text()
+
+		cleaned := cleanInput(text)
+		if len(cleaned) == 0 {
+			continue
+		}
 		
-		fmt.Println(text)
+		fmt.Println(cleaned)
 	}
 }
 
