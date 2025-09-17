d277b33eafabcc072925914dfb93e0265adc1d94
adnant1 <adnantayib8@gmail.com>
2025-09-17 15:48:56 -0400

    Created config struct to track state of the client/commands

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import "github.com/adnant1/pokedexcli/internal/pokeapi"
+
+type config struct {
+	pokeapiClient pokeapi.Client
+	nextLocationAreaURL *string
+	prevLocationAreaURL *string
+}
+
 func main() {
-	startRepl()
+	cfg := config {
+		pokeapiClient: pokeapi.NewClient(),
+		nextLocationAreaURL: nil,
+		prevLocationAreaURL: nil,
+	}
+
+	startRepl(&cfg)
 }
