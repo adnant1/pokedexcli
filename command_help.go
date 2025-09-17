bd95ad0f00ccd792ea0840550291a287bc5f948d
adnant1 <adnantayib8@gmail.com>
2025-09-17 14:51:34 -0400

    Refactored help function to remove hardcoded commands

--- command_help.go
+++ command_help.go
@@ -5,6 +5,9 @@ import "fmt"
 func callbackHelp() {
 	fmt.Println("Welcome to the Pokedex help menu!")
 	fmt.Println("Available commands:")
-	fmt.Println(" - help")
-	fmt.Println(" - exit")
+
+	availableCommands := getCommands()
+	for _, cmd := range availableCommands {
+		fmt.Printf(" - %s: %s\n", cmd.name, cmd.description)
+	}
 }
