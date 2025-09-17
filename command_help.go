0eb94b99b697a237fcaa3824997e1e2e2e6b7890
adnant1 <adnantayib8@gmail.com>
2025-09-17 14:45:03 -0400

    Help command function

---
+++ command_help.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func callbackHelp() {
+	fmt.Println("Welcome to the Pokedex help menu!")
+	fmt.Println("Available commands:")
+	fmt.Println(" - help")
+	fmt.Println(" - exit")
+}
