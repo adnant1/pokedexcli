5e0dc9eb0af0d78a222849820edd7a01e73c4890
adnant1 <adnantayib8@gmail.com>
2025-09-17 16:16:12 -0400

    Another map function to paginate backwards

--- command_map.go
+++ command_map.go
@@ -1,11 +1,33 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
 func callbackMap(cfg *config) error {
 	res, err := cfg.pokeapiClient.GetLocationAreas(cfg.nextLocationAreaURL)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Location Areas:")
+	for _, area := range res.Results {
+		fmt.Printf(" - %s\n", area.Name)
+	}
+
+	cfg.nextLocationAreaURL = res.Next
+	cfg.prevLocationAreaURL = res.Previous
+
+	return nil
+}
+
+func callbackMapb(cfg *config) error {
+	if cfg.prevLocationAreaURL == nil {
+		return errors.New("No previous page")
+	}	
+
+	res, err := cfg.pokeapiClient.GetLocationAreas(cfg.prevLocationAreaURL)
 	if err != nil {
 		return err
 	}
