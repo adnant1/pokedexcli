c5bd02acb0fdacbefe888ef385dd8ad38ea3cf25
adnant1 <adnantayib8@gmail.com>
2025-09-17 14:58:58 -0400

    Added return type error to callback functions

--- command_help.go
+++ command_help.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func callbackHelp() {
+func callbackHelp() error {
 	fmt.Println("Welcome to the Pokedex help menu!")
 	fmt.Println("Available commands:")
 
@@ -10,4 +10,6 @@ func callbackHelp() {
 	for _, cmd := range availableCommands {
 		fmt.Printf(" - %s: %s\n", cmd.name, cmd.description)
 	}
+
+	return nil
 }
