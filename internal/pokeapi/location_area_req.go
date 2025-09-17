632058c5fdad793c54de888e433aa789f655496f
adnant1 <adnantayib8@gmail.com>
2025-09-17 15:25:57 -0400

    Function to call /location-area from the Pokeapi

---
+++ internal/pokeapi/location_area_req.go
@@ -0,0 +1,41 @@
+package pokeapi
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func(c *Client) GetLocationAreas() (LocationAreasResp, error) {
+	endpoint := "/location-area"
+	fullURL := baseURL + endpoint
+
+	req, err := http.NewRequest("GET", fullURL, nil)
+	if err != nil {
+		return LocationAreasResp{}, err
+	}
+
+	res, err := c.httpClient.Do(req)
+	if err != nil {
+		return LocationAreasResp{}, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode > 399 {
+		return LocationAreasResp{}, fmt.Errorf("bad status code: %d", res.StatusCode)
+	}
+
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		return LocationAreasResp{}, err
+	}
+
+	locationAreasResp := LocationAreasResp{}
+	err = json.Unmarshal(data, &locationAreasResp)
+	if err != nil {
+		return LocationAreasResp{}, err
+	}
+
+	return locationAreasResp, nil
+}
