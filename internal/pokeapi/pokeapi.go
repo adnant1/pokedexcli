3dd25822a355a6eeb8a8f5601cd92c96da62759e
adnant1 <adnantayib8@gmail.com>
2025-09-17 15:15:36 -0400

    Moved location areas struct to new file

--- internal/pokeapi/pokeapi.go
+++ internal/pokeapi/pokeapi.go
@@ -18,13 +18,3 @@ func newClient() Client {
 		},
 	}
 }
-
-type LocationAreasResp struct {
-	Count int `json:"count"`
-	Next *string `json:"next"`
-	Previous *string `json:"previous"`
-	Results []struct {
-		Name string `json:"name"`
-		Url string `json:"url"`
-	} `json:"results"`
-}
