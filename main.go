c7b2564a66c0ffa23432e510b6ed1c1b78d54315
adnant1 <adnantayib8@gmail.com>
2025-09-20 19:25:36 -0400

    Input duration of 1 hour in pokeapi client

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/adnant1/pokedexcli/internal/pokeapi"
+import (
+	"time"
+
+	"github.com/adnant1/pokedexcli/internal/pokeapi"
+)
 
 type config struct {
 	pokeapiClient pokeapi.Client
@@ -10,7 +14,7 @@ type config struct {
 
 func main() {
 	cfg := config {
-		pokeapiClient: pokeapi.NewClient(),
+		pokeapiClient: pokeapi.NewClient(time.Hour),
 		nextLocationAreaURL: nil,
 		prevLocationAreaURL: nil,
 	}
