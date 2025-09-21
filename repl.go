7178cd01d6bc98e5297a27a14bd919618830960c
adnant1 <adnantayib8@gmail.com>
2025-09-20 20:10:35 -0400

    Updated callback methods to take in args ...string

--- repl.go
+++ repl.go
@@ -10,7 +10,7 @@ import (
 type cliCommand struct {
 	name string
 	description string
-	callback func(*config) error
+	callback func(*config, ...string) error
 }
 
 func startRepl(cfg *config) {
@@ -27,6 +27,10 @@ func startRepl(cfg *config) {
 		}
 
 		commandName := cleaned[0]
+		args := []string{}
+		if len(cleaned) > 1 {
+			args = cleaned[1:]
+		}
 
 		availableCommands := getCommands()
 
@@ -36,7 +40,7 @@ func startRepl(cfg *config) {
 			continue
 		}
 
-		err := command.callback(cfg)
+		err := command.callback(cfg, args...)
 		if err != nil {
 			fmt.Printf("Error: %s\n", err)
 		}
